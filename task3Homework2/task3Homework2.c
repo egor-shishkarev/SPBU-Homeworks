3f3e7109d62acea2b96c1fd2bd25ef722eaabcdf
egor-shishkarev <egorsiskarev92@gmail.com>
2022-09-24 20:47:22 +0300

    Now function is working correctly

--- task3Homework2/task3Homework2.c
+++ task3Homework2/task3Homework2.c
@@ -3,43 +3,45 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int bubbleSort(long long *arrayOfNumbers, const int lengthOfArray) {
+int bubbleSort(int arrayOfNumbers[], const int lengthOfArray) {
 	for (int i = 0; i < lengthOfArray; ++i) {
 		bool flagOfChanged = false;
-		for (int j = i; j < lengthOfArray - 1; ++j) {
-			if (arrayOfNumbers[j] < arrayOfNumbers[j + 1]) {
-				const long long buffer = arrayOfNumbers[j];
-				arrayOfNumbers[j] = arrayOfNumbers[j + 1];
-				arrayOfNumbers[j + 1] = buffer;
+		for (int j = lengthOfArray - 1; j > i; --j) {
+			if (arrayOfNumbers[j] < arrayOfNumbers[j - 1]) {
+				const int buffer = arrayOfNumbers[j];
+				arrayOfNumbers[j] = arrayOfNumbers[j - 1];
+				arrayOfNumbers[j - 1] = buffer;
 				flagOfChanged = true;
 			}
 		}
-		if !(flagOfChanged) {
+		if (!(flagOfChanged)) {
 			break;
 		}
 	}
-	return arrayOfNumbers
+	return arrayOfNumbers;
 }
 
-int countSort() {
+/*int countSort() {
 
-}
+}*/
 
 int main() {
 	/*clock_t startBubbleSort = clock();
 	clock_t endBubbleSort = clock();
 	clock_t startCountSort = clock();
 	clock_t endCountSort = clock();*/
-	long long arrayOfNumbers[100000] = { 0 };
-	for (int i = 0; i < 100000; ++i) {
-		arrayOfNumbers[i] = srand(clocl(NULL));
+	int arrayOfNumbers[5] = {0};
+	srand(clock());
+	for (int i = 0; i < 5; ++i) {
+		arrayOfNumbers[i] = rand();
 	}
-	for (int i = 0; i < 100000; ++i) {
-		printf("%llu ", arrayOfNumbers[i]);
+	for (int i = 0; i < 5; ++i) {
+		printf("%d ", arrayOfNumbers[i]);
 	}
-	bubbleSort(*arrayOfNumbers, 100000);
-	for (int i = 0; i < 100000; ++i) {
-		printf("%llu ", arrayOfNumbers[i]);
+	bubbleSort(arrayOfNumbers, 5);
+	printf("\n");
+	for (int i = 0; i < 5; ++i) {
+		printf("%d ", arrayOfNumbers[i]);
 	}
 	return 0;
 }
