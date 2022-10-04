eb48d3f84a570da2147f9d511adea87168b14d68
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-04 20:25:14 +0300

    Created and connected the module and the header file to it. Everything is working.

---
+++ Homework4/task2/task2.c
@@ -0,0 +1,88 @@
+﻿#include "smartQuickSort.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h> 
+#include <locale.h>
+#include <time.h>
+#include <stdlib.h>
+
+int verificationIntScanf() {
+	int readValues = 0;
+	while (true) {
+		int correctlyReadValues = scanf("%d", &readValues);
+		while (getchar() != '\n') {
+		}
+		if (correctlyReadValues == 1) {
+			break;
+		}
+		printf("Ошибка ввода. Введите целочисленное значение!\n");
+	}
+	return readValues;
+}
+
+int maxCountInSortArray(int* arrayOfNumbers, const int lengthOfArray) {
+	int element = arrayOfNumbers[0];
+	int countOfElements = 1;
+	int maxCount = 0;
+	int i = 0;
+	while (i < lengthOfArray - 1) {
+		if (arrayOfNumbers[i] != arrayOfNumbers[i + 1]) {
+			if (countOfElements > maxCount) {
+				maxCount = countOfElements;
+				element = arrayOfNumbers[i];
+			}
+			countOfElements = 1;
+		}
+		else {
+			++countOfElements;
+		}
+		++i;
+	}
+	if (arrayOfNumbers[i - 1] == arrayOfNumbers[i] && countOfElements > maxCount) {
+		element = arrayOfNumbers[i];
+	}
+	return element;
+}
+
+int main() {
+	setlocale(LC_ALL, ".1251");
+	printf("Данная программа находит наиболее часто встречающийся элемент в массиве.\nЕсли хотите ввести свой массив - введите 1. \nЕсли хотите увидеть пример - введите 0. => ");
+	int flagOfMode = verificationIntScanf();
+	if (flagOfMode != 1 && flagOfMode != 0) {
+		printf("Значение не было распознано, работа программы завершена.");
+		return 1;
+	}
+	printf("Введите длину массива => ");
+	int numberOfElements = verificationIntScanf();
+	int* arrayOfNumbers = (int*)calloc(numberOfElements, sizeof(int));
+	if (arrayOfNumbers == NULL) {
+		printf("Произошла ошибка, массив не был создан.");
+		return -1;
+	}
+	switch (flagOfMode) {
+	case 0:
+		srand(clock());
+		for (int i = 0; i < numberOfElements; ++i) {
+			arrayOfNumbers[i] = rand() % (10) + 1;
+		}
+		break;
+	case 1:
+		printf("Вводите элементы через enter => ");
+		for (int i = 0; i < numberOfElements; ++i) {
+			arrayOfNumbers[i] = verificationIntScanf();
+		}
+		break;
+	}
+	printf("Созданный массив: \n");
+	for (int i = 0; i < numberOfElements; ++i) {
+		printf("%d ", arrayOfNumbers[i]);
+	}
+	smartQuickSort(arrayOfNumbers, 0, numberOfElements - 1);
+	printf("\nОтсортированный массив: \n");
+	for (int i = 0; i < numberOfElements; ++i) {
+		printf("%d ", arrayOfNumbers[i]);
+	}
+	printf("\nНаиболее часто встречающийся элемент в массиве => %d", maxCountInSortArray(arrayOfNumbers, numberOfElements));
+	free(arrayOfNumbers);
+	return 0;
+}
