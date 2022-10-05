dfd72bccd599e53ca41d4d47c36ec6c49e7d60f4
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-05 21:28:02 +0300

    Add tests

--- Homework3/task3/task3.c
+++ Homework3/task3/task3.c
@@ -34,6 +34,9 @@ void insertionSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
 }
 
 int smartQuickSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
+	if (arrayOfNumbers == NULL || firstIndex > lastIndex || firstIndex < 0 || lastIndex < 0) {
+		return -1;
+	}
 	if ((firstIndex < lastIndex) && (lastIndex - firstIndex + 1) < 10) {
 		int left = firstIndex;
 		int right = lastIndex;
@@ -62,6 +65,9 @@ int smartQuickSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
 }
 
 int maxCountInSortArray(int* arrayOfNumbers, const int lengthOfArray) {
+	if (arrayOfNumbers == NULL || lengthOfArray <= 0) {
+		return NULL;
+	}
 	int element = arrayOfNumbers[0];
 	int countOfElements = 1;
 	int maxCount = 0;
@@ -84,8 +90,38 @@ int maxCountInSortArray(int* arrayOfNumbers, const int lengthOfArray) {
 	return element;
 }
 
+bool testMostFrequentlyElement(int* arrayOfNumbers, const int lengthOfArray, const int rightNumber) {
+	const int result = smartQuickSort(arrayOfNumbers, 0, lengthOfArray - 1);
+	return (result + 1) * (maxCountInSortArray(arrayOfNumbers, lengthOfArray) == rightNumber);
+}
+
+bool test1() {
+	int arrayOfNumbers[6] = { 3, 1, 2, 3, 3, 2 };
+	return testMostFrequentlyElement(arrayOfNumbers, 6, 3);
+}
+
+bool test2() {
+	int arrayOfNumbers[10] = { 7, 5, 6, 1, 2, 7, 12, 4, 4, 1 };
+	return testMostFrequentlyElement(arrayOfNumbers, 10, 1) || testMostFrequentlyElement(arrayOfNumbers, 10, 4) || testMostFrequentlyElement(arrayOfNumbers, 10, 7);
+}
+
+bool test3() {
+	int arrayOfNumbers[1] = { 0 };
+	return testMostFrequentlyElement(arrayOfNumbers, 1, 0);
+}
+
+bool incorrectCase() {
+	int arrayOfNumbers[4] = { 1, 2, 3, 1 };
+	return !(testMostFrequentlyElement(arrayOfNumbers, 0, 1));
+}
+
 int main() {
 	setlocale(LC_ALL, ".1251");
+	if (!(test1() && test2() && test3() && incorrectCase())) {
+		printf("Тесты не пройдены!");
+		return -1;
+	}
+	printf("Тесты пройдены!\n");
 	printf("Данная программа находит наиболее часто встречающийся элемент в массиве.\nЕсли хотите ввести свой массив - введите 1. \nЕсли хотите увидеть пример - введите 0. => ");
 	int flagOfMode = verificationIntScanf();
 	if (flagOfMode != 1 && flagOfMode != 0) {
