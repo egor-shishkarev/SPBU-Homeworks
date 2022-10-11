cae0fd231de14dec38128a48066f56301b06d3ec
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-11 22:33:45 +0300

    Add tests.

--- Homework4/task1/task1.c
+++ Homework4/task1/task1.c
@@ -19,7 +19,19 @@ int verificationIntScanf() {
 	return readValues;
 }
 
-void summaryOfTwoBinary(const long long int first, const long long int second, const maxBite) {
+int sumDisplay(const int* result, const int maxBite) {
+	for (int i = 0; i < 8 * maxBite; ++i) {
+		if (i != 0 && i % 8 == 0) {
+			printf(" ");
+		}
+		printf("%d", *(result + i));
+	}
+}
+
+long long int additionOfTwoBinary(const long long int first, const long long int second, const maxBite, bool showSum) {
+	if (first > 1073741823 || first < -1073741823 || second > 1073741823 || second < -1073741823 || maxBite < 2) {
+		return NULL;
+	}
 	long long int mask = 1;
 	int *result = (int*)malloc((8 * maxBite) * sizeof(int));
 	if (result == NULL) {
@@ -40,26 +52,66 @@ void summaryOfTwoBinary(const long long int first, const long long int second, c
 		buffer = 0;
 		mask <<= 1;
 	} 
-	for (int i = 0; i < 8 * maxBite; ++i) {
-		if (i != 0 && i % 8 == 0) {
-			printf(" ");
-		}
-		printf("%d", result[i]);
+	if (showSum) {
+		sumDisplay(result, maxBite);
 	}
-	printf("\nСумма в десятичной системе счисления: ");
 	long long int sumDecimal = -*(result + 7) * pow(2, (maxBite - 1) * 8);
 	for (int i = 8; i < 8 * maxBite + 1; ++i) {
-		sumDecimal += (int)pow(2, 8 * maxBite - 1  - i) * result[i];
+		sumDecimal += (long long int)pow(2, 8 * maxBite - 1  - i) * *(result + i);
 	}
-	printf("%d", sumDecimal);
 	free(result);
+	return sumDecimal;
+}
+
+bool testSum(const long long int first, const long long int second, const int bites) {
+	if (first > 1073741823 || first < -1073741823 || second > 1073741823 || second < -1073741823 || bites < 2) {
+		return additionOfTwoBinary(first, second, bites, false) == NULL;
+	}
+	return additionOfTwoBinary(first, second, bites, false) == first + second;
+}
+
+bool test1() {
+	long long int firstNumber = 128;
+	long long int secondNumber = 256;
+	return testSum(firstNumber, secondNumber, 3);
+}
+
+bool test2() {
+	long long int firstNumber = -89;
+	long long int secondNumber = -104;
+	return testSum(firstNumber, secondNumber, 2);
+}
+
+bool test3() {
+	long long int firstNumber = 1073741823;
+	long long int secondNumber = 142723;
+	return testSum(firstNumber, secondNumber, 5);
+}
+
+bool incorrectCase() {
+	long long int firstNumber = 1073741824;
+	long long int secondNumber = -1073741824;
+	return testSum(firstNumber, secondNumber, 5);
 }
 
 int main() {
 	setlocale(LC_ALL, ".1251");
+	if (!(test1() && test2() && test3() && incorrectCase())) {
+		printf("Тесты не пройдены!");
+		return -1;
+	}
+	printf("Тесты успешно пройдены!\n");
 	printf("Введите два целых числа в диапазоне от -1073741823 до 1073741823 => ");
 	long long int firstNumber = verificationIntScanf();
+	while (firstNumber > 1073741823 || firstNumber < -1073741823) {
+		printf("Введено неверное значение. Повторите попытку => ");
+		firstNumber = verificationIntScanf();
+	}
 	long long int secondNumber = verificationIntScanf();
+	while (secondNumber > 1073741823 || secondNumber < -1073741823) {
+		printf("Введено неверное значение. Повторите попытку => ");
+		secondNumber = verificationIntScanf();
+	}
 	long long int biteToFirst = (int)floor(log2(2 * abs(firstNumber) + (firstNumber == 0)) / 8) + 2;
 	long long int biteToSecond = (int)floor(log2(2 * abs(secondNumber) + (secondNumber == 0)) / 8) + 2;
 	long long int maxBite = MAX(biteToFirst, biteToSecond);
@@ -92,7 +144,7 @@ int main() {
 		printf("-");
 	}
 	printf("\n");
-	summaryOfTwoBinary(firstNumber, secondNumber, maxBite);
+	printf("\nСумма в десятичной системе счисления: %lli", additionOfTwoBinary(firstNumber, secondNumber, maxBite, true));
 	return 0;
 }
 
