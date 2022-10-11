c8e171df22d36b556cea974048326485f203d65f
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-12 00:51:41 +0300

    Add a function to enter a name and phone number, print all entries, and a prototype for searching for a number by name.

--- Homework4/task3/task3.c
+++ Homework4/task3/task3.c
@@ -1,4 +1,5 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 #include <stdbool.h>
 
@@ -19,24 +20,52 @@ typedef struct {
 	если файла нет — начинать с пустой базы номеров. Размер базы ограничен сотней записей.*/
 
 int addNote() {
-	FILE* file = fopen("phonebook.txt", "w");
+	FILE* file = fopen("phonebook.txt", "a");
 	printf("Введите имя => ");
 	phonebook note;
-	scanf("%s", &note.name);
-	printf("Введите номер без пробелов, тире и '+' => ");
-	verificationIntScanf(note.phone);
-	fprintf(note.name, "%s", file);
-	fprintf("  ", "%s", file);
-	fprintf(note.phone, "%s", file);
-	fprintf("\n", "%s", file);
+	gets(&note.name);
+	printf("Введите номер => ");
+	gets(&note.phone);
+	fprintf(file, "%s", "\n");
+	fprintf(file, "%s", note.name);
+	fprintf(file, "%s", " ");
+	fprintf(file, "%s", note.phone);
+	fprintf(file, "%s", "\0");
 	fclose(file);
 }
 
 int printNotes() {
-	FILE* file = fopen("phonebook.txt", "r");
-	while (!feof(file)) {
+	FILE* file = fopen("phonebook.txt", "r"); 
+	while (!feof(file)) { 
+		char *buffer = (char*)malloc(sizeof(char) * 100); 
+		if (buffer == NULL) {
+			printf("Массив не был создан!");
+			return -1;
+		}
+		fgets(buffer, 100, file);
+		printf(buffer);
+		free(buffer);
+	} 
+	printf("\n");
+	fclose(file);
+}
 
+int searchPhone() {
+	FILE* file = fopen("phonebook.txt", "r");
+	printf("Введите имя для поиска номера => ");
+	while (!feof(file)) {
+		char* buffer = (char*)malloc(sizeof(char) * 100);
+		if (buffer == NULL) {
+			printf("Массив не был создан!");
+			return -1;
+		}
+		fgets(buffer, 100, file);
+		for (int i = 0; i < 100; ++i) {
+			printf("%c", buffer[i]);
+		}
+		free(buffer);
 	}
+	fclose(file);
 }
 
 int workingMode(int mode) {
@@ -49,8 +78,10 @@ int workingMode(int mode) {
 		addNote();
 		break;
 	case 2:
+		printNotes();
 		break;
 	case 3:
+		searchPhone();
 		break;
 	case 4:
 		break;
@@ -78,16 +109,19 @@ int verificationIntScanf() {
 }
 
 int main() {
-	setlocale(LC_ALL, ".1251");
+	setlocale(LC_ALL, "Russian");
 	printf("Добро пожаловать в программу-телефонный справочник!\nВыберите действие:\n");
 	printf("0 - выйти\n1 - добавить запись(имя и телефон)\n2 - распечатать все имеющиеся записи\n\
 3 - найти телефон по имени\n4 - найти имя по телефону\n5 - сохранить текущие данные в файл");
-	printf("Введите цифру => ");
+	printf("\nВведите цифру => ");
 	int mode = verificationIntScanf();
-	while (workingMode(mode) != 0) {
+	while (true) {
 		workingMode(mode);
-		printf("\nВведите цифру => ");
-		int mode = verificationIntScanf();
+		if (mode == 0) {
+			break;
+		}
+		printf("Введите цифру => ");
+		mode = verificationIntScanf();
 	}
 	return 0;
 }
