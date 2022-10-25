1d969e83f6c58e6cf42e5d087818934bbc99fcb6
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-25 21:24:29 +0300

    A commit to match files across branches.

---
+++ Stack/stackModule.c
@@ -0,0 +1,68 @@
+#include "stackModule.h"
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct Node {
+    int value;
+    struct Node* next;
+} Node;
+
+typedef struct Stack {
+    Node* head;
+} Stack;
+
+int push(Stack* stack, int value) {
+    Node* temp = malloc(sizeof(Node));
+    if (temp == NULL) {
+        printf("Память не была выделена.");
+        return -1;
+    }
+    temp->value = value;
+    temp->next = stack->head;
+
+    stack->head = temp;
+    return 0;
+}
+
+int pop(Stack* stack, int* errorCode) {
+    if (stack == NULL) {
+        if (errorCode != NULL) {
+            *errorCode = -1;
+        }
+
+        return 0;
+    }
+    if (errorCode != NULL) {
+        *errorCode = 0;
+    }
+
+    int value = stack->head->value;
+
+    Node* next = stack->head->next;
+    free(stack->head);
+    stack->head = next;
+
+    return value;
+}
+
+bool isEmpty(Stack* stack) {
+    return stack->head == NULL;
+}
+
+void deleteStack(Stack* stack) {
+    while (!isEmpty(stack)) {
+        int errorCode = 0;
+        pop(stack, &errorCode);
+    }
+    free(stack);
+}
+
+Stack* createStack() {
+    Stack* stack = malloc(sizeof(Stack));
+    return stack;
+}
+
+int top(Stack* stack) {
+    return stack->head->value;
+}
+
