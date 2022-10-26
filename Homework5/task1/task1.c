b019a510606382f3974df72323103ff0b28474a7
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-26 17:21:22 +0300

    some changes

--- Homework5/task1/task1.c
+++ Homework5/task1/task1.c
@@ -4,6 +4,7 @@
 #include <malloc.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 #define MaxExpressionLength 50
 
 bool isDigit(char symbol) {
@@ -26,34 +27,56 @@ bool isOperation(char symbol) {
 	return false;
 }
 
-int expressionEvaluation(const char prefixExpression[], const int lengthOfExpression) {
-
+int expressionEvaluation(const char postfixExpression[], const int lengthOfExpression) {
+	int* errorCode = 0;
+	Stack* stack = createStack();
+	for (int i = 0; i < lengthOfExpression; ++i) {
+		const char currentElement = postfixExpression[i];
+		if (isDigit(currentElement)) {
+			push(stack, currentElement);
+		}
+		if (isOperation(currentElement)) {
+			const char firstNumber = pop(stack, errorCode);
+			const char secondNumber = pop(stack, errorCode);
+			switch (currentElement) {
+			case '-': {
+				push(stack, (char)(atoi(firstNumber) - atoi(secondNumber)));
+				break;
+			}
+			case '+': {
+				push(stack, (char)(atoi(firstNumber) + atoi(secondNumber)));
+				break;
+			}
+			case '*': {
+				push(stack, (char)(atoi(firstNumber) * atoi(secondNumber)));
+				break;
+			}
+			case '/':
+				push(stack, (char)(atoi(firstNumber) / atoi(secondNumber)));
+				break;
+			}
+		}
+	}
+	int result = pop(stack, errorCode);
+	return result;
 }
 
 int main() {
 	setlocale(LC_ALL, ".1251");
 	printf("Данная программа позволяет вычислить арифметическое выражение в постфиксной форме.\n\
 Введите не более %d символов в одну строчку в виде '9 8 *' => ", MaxExpressionLength);
-	char postfixExpression[MaxExpressionLength] = {""};
-	if (postfixExpression == NULL) {
-		printf("Память не была выделена.");
-		return -1;
-	}
-	char currentChar = getchar();
+	char postfixExpression[MaxExpressionLength] = { "" };
 	int currentElement = 0;
-	Stack* stack = createStack();
+	char currentChar = getchar();
 	while (currentChar != '\n') {
 		if (currentChar == ' ') {
 			currentChar = getchar();
 			continue;
 		}
-		push(stack, currentChar);
+		postfixExpression[currentElement] = currentChar;
 		++currentElement;
 		currentChar = getchar();
 	}
-	for (int i = 0; i < MaxExpressionLength; ++i) {
-		printf("'%c'", postfixExpression[i]);
-	}
-	printf("%s", isDigit(postfixExpression[0]) ? "Yes" : "No");
+	printf("%d", expressionEvaluation(postfixExpression, currentElement));
 	return 0;
 }
