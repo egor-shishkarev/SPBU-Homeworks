b3703fb097297d65133ad36fb9d9564b6f756b50
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-26 18:22:20 +0300

    Add first task without tests.

---
+++ KR_2/task1/task1.c
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stdbool.h>
+
+bool searchTest() {
+
+}
+
+bool test1(void) {
+	int arrayOfSymbols[257] = {0};
+	int maxCode = findSymbols("test1.txt", arrayOfSymbols);
+
+}
+
+bool test2(void) {
+
+}
+
+bool test3(void) {
+
+}
+
+int findSymbols(const char *fileName, int *arrayOfSymbols) {
+	FILE* file = fopen(fileName, "r");
+	if (file == NULL) {
+		printf("Такого файла нет.");
+		return -1;
+	}
+	int maxCode = -1;
+	while (!feof(file)) {
+		const char currentSymbol = getc(file);
+		const int code = currentSymbol;
+		if (code > maxCode) {
+			maxCode = code;
+		}
+		if (code < 0) {
+			continue;
+		}
+		arrayOfSymbols[code] += 1;
+	}
+	fclose(file);
+	return maxCode;
+}
+
+int main() {
+	setlocale(LC_ALL, ".1251");
+	int arrayOfSymbols[257] = {0};
+	int maxCode = findSymbols("text.txt", arrayOfSymbols);
+	for (int i = 0; i < maxCode + 1; ++i) {
+		if (arrayOfSymbols[i] != 0) {
+			if (i == 10 && arrayOfSymbols[i] != 0) {
+				printf("Символ '\\n' встречается в файле - %d раз\n", arrayOfSymbols[i]);
+				continue;
+			}
+			printf("Символ '%c' встречается в файле - %d раз\n", (char)i, arrayOfSymbols[i]);
+		}
+	}
+	if (maxCode == -1) {
+		printf("В файле нет символов, он пуст.");
+	}
+	return 0;
+}
