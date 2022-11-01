7e47633e31d67ce224e948183e85d2cda356d51b
egor-shishkarev <egorsiskarev92@gmail.com>
2022-11-01 18:15:53 +0300

    Set up the program.

--- Homework5/task3/task3.c
+++ Homework5/task3/task3.c
@@ -8,6 +8,7 @@
 #define MaxExpressionLength 50
 
 bool isDigit(char symbol) {
+	/*return (int)symbol >= 48 && (int)symbol<=57*/
 	char allDigits[11] = { "0123456789" };
 	for (int i = 0; i < 10; ++i) {
 		if (symbol == allDigits[i]) {
@@ -45,12 +46,16 @@ char *conversionFromInfixToPostfix(const char *infixExpression, const int length
 		return;
 	}
 	int currentPosition = 0;
-	bool firstOperation = false;
+	bool firstOperation = true;
+	int operationInBrackets = 0;
 	for (int i = 0; i < lengthOfString; ++i) {
 		const char currentElement = infixExpression[i];
 		if (isDigit(currentElement)) { // Если символ - цифра, просто добавляем в стек для цифр
 			*errorCode = push(stackForDigits, currentElement);
 		} else if (isOperation(currentElement) || currentElement == '(') { // Если символ - операция, начинаем смотреть варианты
+			if (currentElement == '(') {
+				++operationInBrackets;
+			}
 			if (isEmpty(stackForOperations)) { // Если стек пустой - просто добавляем его туда
 				*errorCode = push(stackForOperations, currentElement);
 				continue;
@@ -59,15 +64,14 @@ char *conversionFromInfixToPostfix(const char *infixExpression, const int length
 			if (operationPriority(currentElement) > operationPriority(topStack) || topStack == '(' || currentElement == '(') { // Если приоритет текущей операции выше чем операции из стека, вершина стека ( или текущая операция ( добавляем его
 				*errorCode = push(stackForOperations, currentElement);
 			} else { // Если же приоритет ниже, рассматриваем случаи
-				firstOperation = false;
 				if (currentElement == ')') { // Случай с закрывающей скобкой
 					while (topStack != '(') {
-						if (!firstOperation) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
+						if (operationInBrackets) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
 							postfixExpression[currentPosition + 1] = pop(stackForDigits, errorCode);
 							postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
 							postfixExpression[currentPosition + 2] = pop(stackForOperations, errorCode);
 							currentPosition += 3;
-							firstOperation = true;
+							--operationInBrackets;
 						} else { // Если это вторая и больше операция после ), то достаем одно число и одну операцию
 							postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
 							postfixExpression[currentPosition + 1] = pop(stackForOperations, errorCode);
@@ -77,11 +81,12 @@ char *conversionFromInfixToPostfix(const char *infixExpression, const int length
 					}
 					pop(stackForOperations, *errorCode); // Достаем закрывающую скобку. Она нам не нужна, просто делаем поп
 				} else { // Если это не ), то достаем два числа и операцию
-					while (operationPriority(currentElement) <= operationPriority(topStack) || !isEmpty(stackForDigits) || !isEmpty(stackForOperations)) {
-						if (!firstOperation) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
+					while (operationPriority(currentElement) <= operationPriority(topStack) || !isEmpty(stackForDigits) || !isEmpty(stackForOperations) || top(stackForOperations) != '(') {
+						if (firstOperation) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
 							postfixExpression[currentPosition + 1] = pop(stackForDigits, errorCode);
 							postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
 							postfixExpression[currentPosition + 2] = pop(stackForOperations, errorCode);
+							firstOperation = false;
 							currentPosition += 3;
 						}
 						else { // Если это вторая и больше операция после ), то достаем одно число и одну операцию
@@ -90,19 +95,18 @@ char *conversionFromInfixToPostfix(const char *infixExpression, const int length
 							currentPosition += 2;
 						}
 					}
-					firstOperation = true;
+					*errorCode = push(stackForOperations, currentElement);
 				}
 			}
 		} else if (currentElement == ')') { // Случай с закрывающей скобкой
-			firstOperation = false;
 			char topStack = top(stackForOperations);
 			while (topStack != '(') {
-				if (!firstOperation) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
+				if (operationInBrackets) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
 					postfixExpression[currentPosition + 1] = pop(stackForDigits, errorCode);
 					postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
 					postfixExpression[currentPosition + 2] = pop(stackForOperations, errorCode);
 					currentPosition += 3;
-					firstOperation = true;
+					--operationInBrackets;
 				}
 				else { // Если это вторая и больше операция после ), то достаем одно число и одну операцию
 					postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
@@ -119,19 +123,13 @@ char *conversionFromInfixToPostfix(const char *infixExpression, const int length
 			return NULL;
 		}
 	}
-	if (!isEmpty(stackForOperations)) {
-		if (!firstOperation) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
-			postfixExpression[currentPosition + 1] = pop(stackForDigits, errorCode);
+	while (!isEmpty(stackForOperations)) {
+		if (!isEmpty(stackForDigits)) {
 			postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-			postfixExpression[currentPosition + 2] = pop(stackForOperations, errorCode);
-			currentPosition += 3;
-			firstOperation = true;
+			++currentPosition;
 		}
-		else { // Если это вторая и больше операция после ), то достаем одно число и одну операцию
-			postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-			postfixExpression[currentPosition + 1] = pop(stackForOperations, errorCode);
-			currentPosition += 2;
-		}
+		postfixExpression[currentPosition] = pop(stackForOperations, errorCode);
+		++currentPosition;
 	}
 	deleteStack(stackForDigits);
 	deleteStack(stackForOperations);
@@ -147,7 +145,7 @@ int main() {
 	setlocale(LC_ALL, ".1251");
 	if (!test()) {
 		printf("Тесты не пройдены.");
-		return -1;
+		//return -1;
 	}
 	printf("Тесты успешно пройдены.\n");
 	printf("Введите выражение длиной максимум в %d символов для перевода из инфиксной в постфиксную форму записи =>", MaxExpressionLength);
@@ -166,5 +164,6 @@ int main() {
 	int errorCode = 0;
 	char *postfixExpression = conversionFromInfixToPostfix(infixExpression, currentElement, &errorCode);
 	printf("%s", postfixExpression);
+	free(postfixExpression);
 	return 0;
 }
