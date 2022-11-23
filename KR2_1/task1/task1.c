b6da919a978e7029d219dde755e2b062b8458ee9
egor-shishkarev <egorsiskarev92@gmail.com>
2022-11-23 16:16:09 +0300

    Add first task with tests.

---
+++ KR2_1/task1/task1.c
@@ -0,0 +1,76 @@
+﻿#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <locale.h>
+
+int verificationIntScanf(void) {
+	int readValues = 0;
+	while (true) {
+		int correctlyReadValues = scanf("%d", &readValues);
+		while (getchar() != '\n') {
+		}
+		if (correctlyReadValues == 1) {
+			break;
+		}
+		printf("Было введено нецелочисленное значение! \n");
+	}
+	return readValues;
+}
+
+bool correctCase(void) {
+	List* list = createList();
+	for (int i = 0; i < 500000; ++i) {
+		insertElement(list, i);
+	}
+	for (int i = 499999; i >= 0; --i) {
+		insertElement(list, i);
+	}
+	int errorCode = 0;
+	return isSymethric(list, &errorCode) && !errorCode;
+}
+
+bool incorrectCase(void) {
+	List* list = createList();
+	for (int i = 0; i < 100; ++i) {
+		insertElement(list, i);
+	}
+	insertElement(list, 9);
+	insertElement(list, 10);
+	for (int i = 99; i >= 0; --i) {
+		insertElement(list, i);
+	}
+	int errorCode = 0;
+	return !isSymethric(list, &errorCode) && !errorCode;
+}
+
+int main(void) {
+	setlocale(LC_ALL, ".1251");
+	if (!(correctCase() && incorrectCase())) {
+		printf("Тесты не были пройдены!");
+		return -1;
+	}
+	FILE* file = fopen("example.txt", "r");
+	int errorCode = 0;
+	List* list = createList();
+	while (!feof(file)) {
+		const int currentNumber = 0;
+		if (fscanf(file, "%d", &currentNumber) == -1) {
+			break;
+		}
+		insertElement(list, currentNumber);
+	}
+	fclose(file);
+	bool listIsSymethric = isSymethric(list, &errorCode);
+	if (errorCode) {
+		printf("Произошла ошибка во время проверки на симметричность. В файле не было чисел.");
+		return -1;
+	}
+	printf(listIsSymethric ? "Список симметричен." : "Список не симметричен.");
+	deleteList(list);
+	return 0;
+}
+
+// Проверить список на симметричность. В файле даны натуральные числа, построить 
+// по ним список и определить, симметричен ли он. Например, список из 10 20 30 20 10 симметричен, 
+// 10 20 30 40 20 10 — нет. Требуется использование настоящего списка на указателях, считать всё в массив не пойдёт.
