58237e084f9ad2097d5d56e9d5e63dd5b50fbc98
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-01 20:01:54 +0300

    Fixed minor bugs, added insertion sort function test.

--- Homework3/task1/task1.c
+++ Homework3/task1/task1.c
@@ -19,9 +19,11 @@ int verificationIntScanf() {
 	return readValues;
 }
 
-void insertionSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
+int insertionSort(int* arrayOfNumbers, const int lengthOfArray) {
+	if (arrayOfNumbers == NULL || lengthOfArray <= 0) {
+		return -1;
+	}
 	int currentElement = 1;
-	int lengthOfArray = lastIndex - firstIndex + 1;
 	while (currentElement < lengthOfArray) {
 		while (arrayOfNumbers[currentElement] < arrayOfNumbers[currentElement - 1] && currentElement >= 1) {
 			const int buffer = arrayOfNumbers[currentElement - 1];
@@ -31,6 +33,7 @@ void insertionSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
 		}
 		++currentElement;
 	}
+	return 0;
 }
 
 int smartQuickSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
@@ -59,73 +62,84 @@ int smartQuickSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
 		smartQuickSort(arrayOfNumbers, firstIndex, right);
 		smartQuickSort(arrayOfNumbers, left, lastIndex);
 	} else {
-		insertionSort(arrayOfNumbers, firstIndex, lastIndex);
+		insertionSort(arrayOfNumbers, lastIndex + 1);
 	}
 	return 0;
 }
 
 bool testQuickSort(int* rightArray, int* arrayOfNumbers, const int lengthOfArray) {
 	const int result = smartQuickSort(arrayOfNumbers, 0, lengthOfArray - 1);
-	bool allIsTrue = true;
+	bool allAreTrue = true;
 	for (int i = 0; i < lengthOfArray - 1; ++i) {
 		if (rightArray[i] != arrayOfNumbers[i]) {
-			allIsTrue = false;
+			allAreTrue = false;
 			break;
 		}
 	}
-	return allIsTrue * (result + 1);
+	return allAreTrue && result == 0;
 }
 
-bool incorrectCase() {
+bool testInsertionSort(int* rightArray, int* arrayOfNumbers, const int lengthOfArray) {
+	const int result = insertionSort(arrayOfNumbers, lengthOfArray);
+	bool allAreTrue = true;
+	for (int i = 0; i < lengthOfArray - 1; ++i) {
+		if (rightArray[i] != arrayOfNumbers[i]) {
+			allAreTrue = false;
+			break;
+		}
+	}
+	return allAreTrue && result == 0;
+}
+
+bool incorrectCase(void) {
 	int arrayOfNumbers[2] = { 0, 0 };
 	int rightArray[2] = { 0, 0 };
-	return testQuickSort(rightArray, arrayOfNumbers, -4);
+	return !testQuickSort(rightArray, arrayOfNumbers, -4);
 }
 
-bool test1() {
+bool testSmartQuickSortFunction1(void) {
 	int arrayOfNumbers[5] = { 5,4,3,2,1 };
 	int rightArray[5] = { 1,2,3,4,5 };
 	return testQuickSort(rightArray, arrayOfNumbers, 5);
 }
 
-bool test2() {
+bool testSmartQuickSortFunction2(void) {
+	int arrayOfNumbers[14] = { 4, 5, 9, 2, 1, 8, 10, 4, 7, 3, 8, 9, 11, 13 };
+	int rightArray[14] = { 1, 2, 3, 4, 4, 5, 7, 8, 8, 9, 9, 10, 11, 13 };
+	return testQuickSort(rightArray, arrayOfNumbers, 14);
+}
+
+bool testInsertionSortFunction(void) {
 	int arrayOfNumbers[8] = { 5342, 5436, 164, 547, 2751, 63634, 7457, 789 };
-	int rightArray[8] = { 164, 547, 789, 2751, 5342, 5436, 7457, 63634};
-	return testQuickSort(rightArray, arrayOfNumbers, 8);
+	int rightArray[8] = { 164, 547, 789, 2751, 5342, 5436, 7457, 63634 };
+	return testInsertionSort(rightArray, arrayOfNumbers, 8);
 }
 
-bool test3() {
-	int arrayOfNumbers[10] = { 4, 5, 9, 2, 1, 8, 10, 4, 7, 3};
-	int rightArray[10] = { 1, 2, 3, 4, 4, 5, 7, 8, 9, 10 };
-	return testQuickSort(rightArray, arrayOfNumbers, 10);
-}
-
-int main() {
+int main(void) {
 	setlocale(LC_ALL, ".1251");
-	if (!(test1() && test2() && test3() && !incorrectCase())) {
+	if (!(testInsertionSortFunction() && testSmartQuickSortFunction1() && testSmartQuickSortFunction2() && incorrectCase())) {
 		printf("Тесты не пройдены!");
 		return 1;
 	}
 	printf("Тесты были пройдены успешно!\n");
-	printf("%d", incorectCase());
 	printf("Данная программа позволяет продемонстрировать сортировку qsort с элементами алгоритма сортировки вставками\nЕсли хотите отсортировать свой массив - введите 1. \nЕсли хотите увидеть пример сортировки - введите 0. => ");
-	int flagOfMode = verificationIntScanf();
+	const int flagOfMode = verificationIntScanf();
 	if (flagOfMode != 1 && flagOfMode != 0) {
 		printf("Значение не было распознано, работа программы завершена.");
 		return 1;
 	}
 	printf("Введите длину массива, который хотите отсортировать вставками => ");
-	int numberOfElements = verificationIntScanf();
-	int* arrayOfNumbers = (int*)calloc(numberOfElements, sizeof(int));
+	const int numberOfElements = verificationIntScanf();
+	int* arrayOfNumbers = calloc(numberOfElements, sizeof(int));
 	if (arrayOfNumbers == NULL) {
 		printf("Произошла ошибка, массив не был создан.");
 		return -1;
 	}
 	switch (flagOfMode) {
 	case 0:
-		srand(clock());
+		srand(time(NULL));
 		for (int i = 0; i < numberOfElements; ++i) {
-			arrayOfNumbers[i] = rand() % (500) + 1;
+			arrayOfNumbers[i] = rand() % 500 + 1;
 		}
 		break;
 	case 1:
