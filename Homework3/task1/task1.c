47fa3b0e5da5829a5aee43485e46c12173d6dc42
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-02 15:20:13 +0300

    Change insertionSort function

--- Homework3/task1/task1.c
+++ Homework3/task1/task1.c
@@ -19,13 +19,13 @@ int verificationIntScanf(void) {
     return readValues;
 }
 
-int insertionSort(int* arrayOfNumbers, const int lengthOfArray) {
-    if (arrayOfNumbers == NULL || lengthOfArray <= 0) {
+int insertionSort(int* arrayOfNumbers, const int firstIndex, const int lastIndex) {
+    if (arrayOfNumbers == NULL || firstIndex < 0 || lastIndex < 0 || lastIndex < firstIndex) {
         return -1;
     }
-    int currentElement = 1;
-    while (currentElement < lengthOfArray) {
-        while (arrayOfNumbers[currentElement] < arrayOfNumbers[currentElement - 1] && currentElement >= 1) {
+    int currentElement = firstIndex + 1;
+    while (currentElement <= lastIndex) {
+        while (arrayOfNumbers[currentElement] < arrayOfNumbers[currentElement - 1] && currentElement >= firstIndex + 1) {
             const int buffer = arrayOfNumbers[currentElement - 1];
             arrayOfNumbers[currentElement - 1] = arrayOfNumbers[currentElement];
             arrayOfNumbers[currentElement] = buffer;
@@ -40,30 +40,29 @@ int smartQuickSort(int* arrayOfNumbers, int firstIndex, int lastIndex) {
     if (arrayOfNumbers == NULL || firstIndex > lastIndex || firstIndex < 0 || lastIndex < 0) {
         return -1;
     }
-    if ((firstIndex < lastIndex) && (lastIndex - firstIndex + 1) > 10) {
-        int left = firstIndex;
-        int right = lastIndex;
-        int middle = arrayOfNumbers[(left + right) / 2];
-        do {
-            while (arrayOfNumbers[left] < middle) {
-                ++left;
-            }
-            while (arrayOfNumbers[right] > middle) {
-                --right;
-            }
-            if (left <= right) {
-                const int buffer = arrayOfNumbers[left];
-                arrayOfNumbers[left] = arrayOfNumbers[right];
-                arrayOfNumbers[right] = buffer;
-                ++left;
-                --right;
-            }
-        } while (left <= right);
-        smartQuickSort(arrayOfNumbers, firstIndex, right);
-        smartQuickSort(arrayOfNumbers, left, lastIndex);
-    } else {
-        insertionSort(arrayOfNumbers, lastIndex + 1);
+    int left = firstIndex;
+    int right = lastIndex;
+    int middle = arrayOfNumbers[(left + right) / 2];
+    if (lastIndex - firstIndex < 9) {
+        return insertionSort(arrayOfNumbers, firstIndex, lastIndex);
     }
+    do {
+        while (arrayOfNumbers[left] < middle) {
+            ++left;
+        }
+        while (arrayOfNumbers[right] > middle) {
+            --right;
+        }
+        if (left <= right) {
+            const int buffer = arrayOfNumbers[left];
+            arrayOfNumbers[left] = arrayOfNumbers[right];
+            arrayOfNumbers[right] = buffer;
+            ++left;
+            --right;
+        }
+    } while (left <= right);
+    smartQuickSort(arrayOfNumbers, firstIndex, right);
+    smartQuickSort(arrayOfNumbers, left, lastIndex);
     return 0;
 }
 
@@ -80,7 +79,7 @@ bool testQuickSort(int* rightArray, int* arrayOfNumbers, const int lengthOfArray
 }
 
 bool testInsertionSort(int* rightArray, int* arrayOfNumbers, const int lengthOfArray) {
-    const int result = insertionSort(arrayOfNumbers, lengthOfArray);
+    const int result = insertionSort(arrayOfNumbers, 0, lengthOfArray - 1);
     bool allAreTrue = true;
     for (int i = 0; i < lengthOfArray; ++i) {
         if (rightArray[i] != arrayOfNumbers[i]) {
