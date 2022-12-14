ac2939392a8aa189dcdd46191d426e3f64af3ee2
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-14 23:23:32 +0300

    Change functions and tests for right

--- Homework4/task2/task2.c
+++ Homework4/task2/task2.c
@@ -6,116 +6,119 @@
 #include <time.h>
 #include <stdlib.h>
 
-int verificationIntScanf() {
-	int readValues = 0;
-	while (true) {
-		int correctlyReadValues = scanf("%d", &readValues);
-		while (getchar() != '\n') {
-		}
-		if (correctlyReadValues == 1) {
-			break;
-		}
-		printf("Ошибка ввода. Введите целочисленное значение!\n");
-	}
-	return readValues;
+int verificationIntScanf(void) {
+    int readValues = 0;
+    while (true) {
+        int correctlyReadValues = scanf("%d", &readValues);
+        while (getchar() != '\n') {
+        }
+        if (correctlyReadValues == 1) {
+            break;
+        }
+        printf("Ошибка ввода. Введите целочисленное значение!\n");
+    }
+    return readValues;
 }
 
 int maxCountInSortArray(int* arrayOfNumbers, const int lengthOfArray) {
-	if (arrayOfNumbers == NULL || lengthOfArray <= 0) {
-		return NULL;
-	}
-	int element = arrayOfNumbers[0];
-	int countOfElements = 1;
-	int maxCount = 0;
-	int i = 0;
-	while (i < lengthOfArray - 1) {
-		if (arrayOfNumbers[i] != arrayOfNumbers[i + 1]) {
-			if (countOfElements > maxCount) {
-				maxCount = countOfElements;
-				element = arrayOfNumbers[i];
-			}
-			countOfElements = 1;
-		}
-		else {
-			++countOfElements;
-		}
-		++i;
-	}
-	if (arrayOfNumbers[i - 1] == arrayOfNumbers[i] && countOfElements > maxCount) {
-		element = arrayOfNumbers[i];
-	}
-	return element;
+    if (arrayOfNumbers == NULL || lengthOfArray <= 0) {
+        return NULL;
+    }
+    int element = arrayOfNumbers[0];
+    int countOfElements = 0;
+    int maxCount = 1;
+    int currentElement = element;
+    for (int i = 0; i < lengthOfArray; ++i) {
+        if (arrayOfNumbers[i] == currentElement) {
+            ++countOfElements;
+        }
+        else {
+            if (countOfElements > maxCount) {
+                maxCount = countOfElements;
+                element = currentElement;
+            }
+            countOfElements = 1;
+            currentElement = arrayOfNumbers[i];
+        }
+    }
+    if (countOfElements > maxCount) {
+        element = arrayOfNumbers[lengthOfArray - 1];
+    }
+    return element;
+}
+
+bool testQuickSort(void) {
+    int arrayOfNumbers[10] = { 1, 6, 5, 3, 2, 6, 4, 8, 0, -5 };
+    const int rightArray[10] = { -5, 0, 1, 2, 3, 4, 5, 6, 6, 8 };
+    const int result = smartQuickSort(arrayOfNumbers, 0, 9);
+    bool allAreTrue = true;
+    for (int i = 0; i < 10; ++i) {
+        if (rightArray[i] != arrayOfNumbers[i]) {
+            allAreTrue = false;
+            break;
+        }
+    }
+    return allAreTrue && result == 0;
+}
+
+bool testInsertionSort(void) {
+    int arrayOfNumbers[10] = { 1, 6, 5, 3, 2, 6, 4, 8, 0, -5 };
+    const int rightArray[10] = { -5, 0, 1, 2, 3, 4, 5, 6, 6, 8 };
+    const int result = insertionSort(arrayOfNumbers, 0, 9);
+    bool allAreTrue = true;
+    for (int i = 0; i < 10; ++i) {
+        if (rightArray[i] != arrayOfNumbers[i]) {
+            allAreTrue = false;
+            break;
+        }
+    }
+    return allAreTrue && result == 0;
 }
 
 bool testMostFrequentlyElement(int* arrayOfNumbers, const int lengthOfArray, const int rightNumber) {
-	const int result = smartQuickSort(arrayOfNumbers, 0, lengthOfArray - 1);
-	return (result + 1) * (maxCountInSortArray(arrayOfNumbers, lengthOfArray) == rightNumber);
+    const int result = smartQuickSort(arrayOfNumbers, 0, lengthOfArray - 1);
+    return result == 0 && maxCountInSortArray(arrayOfNumbers, lengthOfArray) == rightNumber;
 }
 
-bool test1() {
-	int arrayOfNumbers[6] = { 3, 1, 2, 3, 3, 2 };
-	return testMostFrequentlyElement(arrayOfNumbers, 6, 3);
+bool correctCase(void) {
+    int arrayOfNumbers[10] = { 7, 5, 6, 1, 2, 7, 12, 4, 4, 1 };
+    return testMostFrequentlyElement(arrayOfNumbers, 10, 1) || testMostFrequentlyElement(arrayOfNumbers, 10, 4) || testMostFrequentlyElement(arrayOfNumbers, 10, 7);
 }
 
-bool test2() {
-	int arrayOfNumbers[10] = { 7, 5, 6, 1, 2, 7, 12, 4, 4, 1 };
-	return testMostFrequentlyElement(arrayOfNumbers, 10, 1) || testMostFrequentlyElement(arrayOfNumbers, 10, 4) || testMostFrequentlyElement(arrayOfNumbers, 10, 7);
+bool incorrectCase(void) {
+    int arrayOfNumbers[4] = { 1, 2, 3, 1 };
+    return !testMostFrequentlyElement(arrayOfNumbers, 0, 1);
 }
 
-bool test3() {
-	int arrayOfNumbers[1] = { 0 };
-	return testMostFrequentlyElement(arrayOfNumbers, 1, 0);
-}
-
-bool incorrectCase() {
-	int arrayOfNumbers[4] = { 1, 2, 3, 1 };
-	return !(testMostFrequentlyElement(arrayOfNumbers, 0, 1));
-}
-
-int main() {
-	setlocale(LC_ALL, ".1251");
-	if (!(test1() && test2() && test3() && incorrectCase())) {
-		printf("Тесты не пройдены!");
-		return -1;
-	}
-	printf("Тесты пройдены!\n");
-	printf("Данная программа находит наиболее часто встречающийся элемент в массиве.\nЕсли хотите ввести свой массив - введите 1. \nЕсли хотите увидеть пример - введите 0. => ");
-	int flagOfMode = verificationIntScanf();
-	if (flagOfMode != 1 && flagOfMode != 0) {
-		printf("Значение не было распознано, работа программы завершена.");
-		return 1;
-	}
-	printf("Введите длину массива => ");
-	int numberOfElements = verificationIntScanf();
-	int* arrayOfNumbers = (int*)calloc(numberOfElements, sizeof(int));
-	if (arrayOfNumbers == NULL) {
-		printf("Произошла ошибка, массив не был создан.");
-		return -1;
-	}
-	switch (flagOfMode) {
-	case 0:
-		srand(clock());
-		for (int i = 0; i < numberOfElements; ++i) {
-			arrayOfNumbers[i] = rand() % (10) + 1;
-		}
-		break;
-	case 1:
-		printf("Вводите элементы через enter => ");
-		for (int i = 0; i < numberOfElements; ++i) {
-			arrayOfNumbers[i] = verificationIntScanf();
-		}
-		break;
-	}
-	printf("Созданный массив: \n");
-	for (int i = 0; i < numberOfElements; ++i) {
-		printf("%d ", arrayOfNumbers[i]);
-	}
-	smartQuickSort(arrayOfNumbers, 0, numberOfElements - 1);
-	printf("\nОтсортированный массив: \n");
-	for (int i = 0; i < numberOfElements; ++i) {
-		printf("%d ", arrayOfNumbers[i]);
-	}
-	printf("\nНаиболее часто встречающийся элемент в массиве => %d", maxCountInSortArray(arrayOfNumbers, numberOfElements));
-	free(arrayOfNumbers);
-	return 0;
+int main(void) {
+    setlocale(LC_ALL, ".1251");
+    if (!(correctCase() && incorrectCase() && testInsertionSort() && testQuickSort())) {
+        printf("Тесты не пройдены!");
+        return -1;
+    }
+    printf("Тесты пройдены!\n");
+    printf("Данная программа находит наиболее часто встречающийся элемент в массиве.");
+    FILE* file = fopen("array.txt", "r");
+    const int numberOfElements = 0;
+    fscanf(file, "%d", &numberOfElements);
+    int* arrayOfNumbers = calloc(numberOfElements, sizeof(int));
+    if (arrayOfNumbers == NULL) {
+        printf("Произошла ошибка, массив не был создан.");
+        return -1;
+    }
+    for (int i = 0; i < numberOfElements; ++i) {
+        fscanf(file, "%d", &arrayOfNumbers[i]);
+    }
+    printf("Созданный массив: \n");
+    for (int i = 0; i < numberOfElements; ++i) {
+        printf("%d ", arrayOfNumbers[i]);
+    }
+    smartQuickSort(arrayOfNumbers, 0, numberOfElements - 1);
+    printf("\nОтсортированный массив: \n");
+    for (int i = 0; i < numberOfElements; ++i) {
+        printf("%d ", arrayOfNumbers[i]);
+    }
+    printf("\nНаиболее часто встречающийся элемент в массиве => %d", maxCountInSortArray(arrayOfNumbers, numberOfElements));
+    free(arrayOfNumbers);
+    return 0;
 }
