d35dfa2b57f04404680bafdee56da4fb7d6be156
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-14 23:55:07 +0300

    Add algorithm Boyer-Moore

--- Homework11/task1/task1.c
+++ Homework11/task1/task1.c
@@ -1,6 +1,16 @@
 ﻿#include <stdio.h>
+#include <malloc.h>
 
 int main(void) {
-
-	return 0;
+    FILE* file = fopen("text.txt", "r");
+    int currentLine = 1;
+    while (!feof(file)) {
+        char* buffer = calloc(1000, sizeof(char));
+        if (fscanf(file, "%s", buffer) == -1) {
+            free(buffer);
+            break;
+        }
+    }
+    
+    return 0;
 }
