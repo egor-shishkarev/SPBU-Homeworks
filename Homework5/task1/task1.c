2324ed14e74ee305e3da05963b81859d36ee40bb
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-23 16:41:23 +0300

    Add errorCodes and exceptions

--- Homework5/task1/task1.c
+++ Homework5/task1/task1.c
@@ -32,7 +32,15 @@ int evaluateExpression(const char postfixExpression[], const int lengthOfExpress
 		}
 		if (isOperation(currentElement)) {
 			const int secondNumber = pop(stack, errorCode);
+			if (*errorCode) {
+				deleteStack(stack);
+				return -1;
+			}
 			const int firstNumber = pop(stack, errorCode);
+			if (*errorCode) {
+				deleteStack(stack);
+				return -1;
+			}
 			switch (currentElement) {
 			case '-': {
 				push(stack, firstNumber - secondNumber);
@@ -54,24 +62,43 @@ int evaluateExpression(const char postfixExpression[], const int lengthOfExpress
 		}
 	}
 	int result = pop(stack, errorCode);
+	bool empty = isEmpty(stack);
 	if (*errorCode) {
 		deleteStack(stack);
 		return -1;
 	}
 	deleteStack(stack);
-	return result;
+	if (empty) {
+		return result;
+	}
+	*errorCode = -2;
+	return -1;
 }
 
 bool correctCase(void) {
 	char postfixExpression[MAX_EXPRESSION_LENGTH] = { "96-12+*" };
 	int errorCode = 0;
 	const int result = evaluateExpression(postfixExpression, 7, &errorCode);
-	return result == 9;
+	return result == 9 && errorCode == 0;
+}
+
+bool incorrectCaseStack(void) {
+	char postfixExpression[MAX_EXPRESSION_LENGTH] = { "-96" };
+	int errorCode = 0;
+	const int result = evaluateExpression(postfixExpression, 4, &errorCode);
+	return result == -1 && errorCode == -1;
+}
+
+bool incorrectCaseExpression(void) {
+	char postfixExpression[MAX_EXPRESSION_LENGTH] = { "5566++" };
+	int errorCode = 0;
+	const int result = evaluateExpression(postfixExpression, 7, &errorCode);
+	return result == -1 && errorCode == -2;
 }
 
 int main(void) {
 	setlocale(LC_ALL, ".1251");
-	if (!correctCase()) {
+	if (!correctCase() && incorrectCaseStack() && incorrectCaseExpression()) {
 		printf("Тесты были провалены.");
 		return -1;
 	}
@@ -92,8 +119,12 @@ int main(void) {
 	}
 	int errorCode = 0;
 	const int result = evaluateExpression(postfixExpression, currentElement, &errorCode);
-	if (errorCode) {
-		printf("При вычислении возникла ошибка.");
+	if (errorCode == -1) {
+		printf("Произошла ошибка со стэком (Создание или попытка взять элемент из пустого стэка)");
+		return -1;
+	}
+	if (errorCode == -2) {
+		printf("Выражение введено неверно! Проверьте перед вводом.");
 		return -1;
 	}
 	printf("Результат вычисления выражения - %d", result);
