e57574311cd0ce15c09bb3c62548431d3993d5bd
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-23 11:28:57 +0300

    Add additional empty lines

--- Stack/stackModule.h
+++ Stack/stackModule.h
@@ -1,17 +1,25 @@
 #pragma once
+
 #include "typeStack.h"
 #include <stdbool.h>
+
 // Структура, содержащая другую структуру - Node, которая в свою очередь содержит два поля: значение и адрес следующего элемента в "стеке".
 typedef struct Stack Stack;
+
 // Функция для заполнения стека. Элемент добавляется в верх стека.
 int push(Stack* stack, uncertainType element);
+
 // Функция, позволяющая "вытащить" элемент с верха стека.
 int pop(Stack* stack, int* errorCode);
+
 // Функция проверки стека на отсутствие элементов.
 bool isEmpty(Stack* stack);
+
 // Функция удаления стека и очищения памяти, выделенной под указатель.
 void deleteStack(Stack* stack);
+
 // Функция создания стека.
 Stack* createStack(void);
+
 // Функция получения элемента с верха стека. Не достаёт элемент.
 int top(Stack* stack);
