deb8730362e395709ed531408f5226cde8d8f1ef
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-24 18:35:50 +0300

    In work!

--- Homework5/task3/task3.c
+++ Homework5/task3/task3.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#define MaxExpressionLength 50
+#define MAX_EXPRESSION_LENGTH 50
 
 bool isDigit(char symbol) {
 	return symbol >= '0' && symbol <= '9';
@@ -24,101 +24,33 @@ int operationPriority(const char symbol) {
 	return 0;
 }
 
-char *conversionFromInfixToPostfix(const char *infixExpression, const int lengthOfString, int *errorCode) {
-	char *postfixExpression = calloc(MaxExpressionLength + 1, sizeof(char)); 
-	Stack* stackForDigits = createStack();
-	Stack* stackForOperations = createStack();
-	if (stackForDigits == NULL || stackForOperations == NULL) {
-		printf("Память не была выделена.");
-		return;
+char* conversionFromInfixToPostfix(const char *infixExpression, const int lengthOfString, int *errorCode) {
+	char *postfixExpression = calloc(MAX_EXPRESSION_LENGTH + 1, sizeof(char));
+	if (postfixExpression == NULL) {
+		*errorCode = -1;
+		return NULL;
 	}
-	int currentPosition = 0;
-	bool firstOperation = true;
-	int operationInBrackets = 0;
+	Stack* stackForOperations = createStack();
+	if (stackForOperations == NULL) {
+		*errorCode = -1;
+		free(postfixExpression);
+		return NULL;
+	}
+	int currentIndex = 0;
 	for (int i = 0; i < lengthOfString; ++i) {
-		const char currentElement = infixExpression[i];
-		if (isDigit(currentElement)) { // Если символ - цифра, просто добавляем в стек для цифр
-			*errorCode = push(stackForDigits, currentElement);
-		} else if (isOperation(currentElement) || currentElement == '(') { // Если символ - операция, начинаем смотреть варианты
-			if (currentElement == '(') {
-				++operationInBrackets;
-			}
-			if (isEmpty(stackForOperations)) { // Если стек пустой - просто добавляем его туда
-				*errorCode = push(stackForOperations, currentElement);
-				continue;
-			}
-			char topStack = top(stackForOperations);
-			if (operationPriority(currentElement) > operationPriority(topStack) || topStack == '(' || currentElement == '(') { // Если приоритет текущей операции выше чем операции из стека, вершина стека ( или текущая операция ( добавляем его
-				*errorCode = push(stackForOperations, currentElement);
-			} else { // Если же приоритет ниже, рассматриваем случаи
-				if (currentElement == ')') { // Случай с закрывающей скобкой
-					while (topStack != '(') {
-						if (operationInBrackets) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
-							postfixExpression[currentPosition + 1] = pop(stackForDigits, errorCode);
-							postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-							postfixExpression[currentPosition + 2] = pop(stackForOperations, errorCode);
-							currentPosition += 3;
-							--operationInBrackets;
-						} else { // Если это вторая и больше операция после ), то достаем одно число и одну операцию
-							postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-							postfixExpression[currentPosition + 1] = pop(stackForOperations, errorCode);
-							currentPosition += 2;
-						}
-						topStack = top(stackForOperations);
-					}
-					pop(stackForOperations, *errorCode); // Достаем закрывающую скобку. Она нам не нужна, просто делаем поп
-				} else { // Если это не ), то достаем два числа и операцию
-					while (operationPriority(currentElement) <= operationPriority(topStack) || !isEmpty(stackForDigits) || !isEmpty(stackForOperations) || top(stackForOperations) != '(') {
-						if (firstOperation) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
-							postfixExpression[currentPosition + 1] = pop(stackForDigits, errorCode);
-							postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-							postfixExpression[currentPosition + 2] = pop(stackForOperations, errorCode);
-							firstOperation = false;
-							currentPosition += 3;
-						}
-						else { // Если это вторая и больше операция после ), то достаем одно число и одну операцию
-							postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-							postfixExpression[currentPosition + 1] = pop(stackForOperations, errorCode);
-							currentPosition += 2;
-						}
-					}
-					*errorCode = push(stackForOperations, currentElement);
-				}
-			}
-		} else if (currentElement == ')') { // Случай с закрывающей скобкой
-			char topStack = top(stackForOperations);
-			while (topStack != '(') {
-				if (operationInBrackets) { // Если операция сразу же после скобки, то достаем два числа и одну операцию
-					postfixExpression[currentPosition + 1] = pop(stackForDigits, errorCode);
-					postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-					postfixExpression[currentPosition + 2] = pop(stackForOperations, errorCode);
-					currentPosition += 3;
-					--operationInBrackets;
-				}
-				else { // Если это вторая и больше операция после ), то достаем одно число и одну операцию
-					postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-					postfixExpression[currentPosition + 1] = pop(stackForOperations, errorCode);
-					currentPosition += 2;
-				}
-				topStack = top(stackForOperations);
-			}
-			pop(stackForOperations, errorCode); // Достаем закрывающую скобку. Она нам не нужна, просто делаем поп
+		const char symbol = infixExpression[i];
+		if (isDigit(symbol)) {
+			postfixExpression[currentIndex] = symbol;
+			continue;
+		} else if (isOperation(symbol)) {
+			//
 		} else {
-			printf("Неизвестный элемент: ни операция, ни цифра. Прекращение работы.");
-			deleteStack(stackForDigits);
+			*errorCode = -1;
+			free(postfixExpression);
 			deleteStack(stackForOperations);
 			return NULL;
 		}
 	}
-	while (!isEmpty(stackForOperations)) {
-		if (!isEmpty(stackForDigits)) {
-			postfixExpression[currentPosition] = pop(stackForDigits, errorCode);
-			++currentPosition;
-		}
-		postfixExpression[currentPosition] = pop(stackForOperations, errorCode);
-		++currentPosition;
-	}
-	deleteStack(stackForDigits);
 	deleteStack(stackForOperations);
 	return postfixExpression;
 }
@@ -128,15 +60,15 @@ bool test(void) {
 	return !strcmp(conversionFromInfixToPostfix("(1+1)*2", 7, &errorCode), "11+2*");
 }
 
-int main() {
+int main(void) {
 	setlocale(LC_ALL, ".1251");
 	if (!test()) {
 		printf("Тесты не пройдены.");
-		//return -1;
+		return -1;
 	}
 	printf("Тесты успешно пройдены.\n");
-	printf("Введите выражение длиной максимум в %d символов для перевода из инфиксной в постфиксную форму записи =>", MaxExpressionLength);
-	char infixExpression[MaxExpressionLength] = { "" };
+	printf("Введите выражение длиной максимум в %d символов для перевода из инфиксной в постфиксную форму записи =>", MAX_EXPRESSION_LENGTH);
+	char infixExpression[MAX_EXPRESSION_LENGTH] = { "" };
 	int currentElement = 0;
 	char currentChar = getchar();
 	while (currentChar != '\n') {
@@ -150,6 +82,11 @@ int main() {
 	}
 	int errorCode = 0;
 	char *postfixExpression = conversionFromInfixToPostfix(infixExpression, currentElement, &errorCode);
+	if (errorCode == -1) {
+		printf("Произошла ошибка!");
+		free(postfixExpression);
+		return -1;
+	}
 	printf("%s", postfixExpression);
 	free(postfixExpression);
 	return 0;
