d9e26e311e0d8f888621b1182e84bc3388af55d7
egor-shishkarev <egorsiskarev92@gmail.com>
2022-12-24 18:19:21 +0300

    Redisgn the program, add tests

--- Homework4/task1/task1.c
+++ Homework4/task1/task1.c
@@ -4,147 +4,123 @@
 #include <malloc.h>
 #include <math.h>
 #include <stdlib.h>
+#define BINARY_SIZE sizeof(int) * 8
 
 int verificationIntScanf(void) {
-	int readValues = 0;
-	while (true) {
-		int correctlyReadValues = scanf("%d", &readValues);
-		while (getchar() != '\n') {
-		}
-		if (correctlyReadValues == 1) {
-			break;
-		}
-		printf("Ошибка ввода. Введите целочисленное значение!\n");
-	}
-	return readValues;
+    int readValues = 0;
+    while (true) {
+        int correctlyReadValues = scanf("%d", &readValues);
+        while (getchar() != '\n') {
+        }
+        if (correctlyReadValues == 1) {
+            break;
+        }
+        printf("Ошибка ввода. Введите целочисленное значение!\n");
+    }
+    return readValues;
 }
 
-int sumDisplay(const int* result, const int maxBite) {
-	for (int i = 0; i < 8 * maxBite; ++i) {
-		if (i != 0 && i % 8 == 0) {
-			printf(" ");
-		}
-		printf("%d", *(result + i));
-	}
+void printBinary(const short* binaryNumber, const unsigned int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d", binaryNumber[i]);
+        if ((i + 1) % 8 == 0 ) {
+            printf(" ");
+        }
+    }
+    printf("\n");
 }
 
-long long int additionOfTwoBinary(const long long int first, const long long int second, const int maxBite, bool showSum) {
-	if (first > 1073741823 || first < -1073741823 || second > 1073741823 || second < -1073741823 || maxBite < 2) {
-		return NULL;
-	}
-	long long int mask = 1;
-	int *result = (int*)malloc((8 * maxBite) * sizeof(int));
-	if (result == NULL) {
-		printf("Массив не был создан.");
-		return -1;
-	}
-	int buffer = 0;
-	for (int i = 8 * maxBite - 1; i >= 0; --i) {
-		const int digitFirst = first & mask ? 1 : 0;
-		const int digitSecond = second & mask ? 1 : 0;
-		if (digitFirst + digitSecond + buffer >= 2) {
-			result[i] = (digitFirst + digitSecond + buffer) % 2;
-			buffer = 1;
-			mask <<= 1;
-			continue;
-		}
-		result[i] = (digitFirst + digitSecond + buffer) % 2;
-		buffer = 0;
-		mask <<= 1;
-	} 
-	if (showSum) {
-		sumDisplay(result, maxBite);
-	}
-	long long int sumDecimal = -*(result + 7) * pow(2, (maxBite - 1) * 8);
-	for (int i = 8; i < 8 * maxBite + 1; ++i) {
-		sumDecimal += (long long int)pow(2, 8 * maxBite - 1  - i) * *(result + i);
-	}
-	free(result);
-	return sumDecimal;
+void binaryRepresentation(int number, short* repressentation, const unsigned int size) {
+    for (int i = size - 1; i >= 0; --i) {
+        repressentation[i] = (short)(number & 1);
+        number = number >> 1;
+    }
 }
 
-bool testSum(const long long int first, const long long int second, const int bites) {
-	if (first > 1073741823 || first < -1073741823 || second > 1073741823 || second < -1073741823 || bites < 2) {
-		return additionOfTwoBinary(first, second, bites, false) == NULL;
-	}
-	return additionOfTwoBinary(first, second, bites, false) == first + second;
+void binarySum(const short* firstBinary, const short* secondBinary, short* sum, const unsigned int size) {
+    bool overflow = false;
+    for (int i = size - 1; i >= 0; --i) {
+        sum[i] = (short) (firstBinary[i] ^ secondBinary[i] ^ overflow);
+        overflow = (firstBinary[i] + secondBinary[i] + overflow) > 1 ? true : false;
+    }
 }
 
-bool test1(void) {
-	long long int firstNumber = 128;
-	long long int secondNumber = 256;
-	return testSum(firstNumber, secondNumber, 3);
+int decimalRepresentation(short* binaryNumber, const unsigned int size) {
+    int sum = 0;
+    int mask = 1;
+    for (int i = size - 1; i > 0; --i) {
+        sum += binaryNumber[i] * mask;
+        mask <<= 1;
+    }
+    sum -= binaryNumber[0] * mask;
+    return sum;
 }
 
-bool test2(void) {
-	long long int firstNumber = -89;
-	long long int secondNumber = -104;
-	return testSum(firstNumber, secondNumber, 2);
+bool correspondenceTest(const short* binaryNumber, const short* right, const unsigned int size) {
+    for (int i = 0; i < size; ++i) {
+        if (binaryNumber[i] != right[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-bool test3(void) {
-	long long int firstNumber = 1073741823;
-	long long int secondNumber = 142723;
-	return testSum(firstNumber, secondNumber, 5);
-}
+bool test(void) {
+    int first = 117;
+    int second = -7;
+    int third = -117;
 
-bool incorrectCase(void) {
-	long long int firstNumber = 1073741824;
-	long long int secondNumber = -1073741824;
-	return testSum(firstNumber, secondNumber, 5);
+    short binaryFirst[10] = { 0 };
+    short binarySecond[10] = { 0 };
+    short binaryThird[10] = { 0 };
+    binaryRepresentation(first, binaryFirst, 10);
+    binaryRepresentation(second, binarySecond, 10);
+    binaryRepresentation(third, binaryThird, 10);
+
+    short rightFirst[10] = {0, 0, 0, 1, 1, 1, 0, 1, 0, 1};
+    short rightSecond[10] = { 1, 1, 1, 1, 1, 1, 1, 0, 0, 1 };
+    short rightThird[10] = { 1, 1, 1, 0, 0, 0, 1, 0, 1, 1 };
+
+    short firstAddition[10] = { 0 };
+    short secondAddition[10] = { 0 };
+    short thirdAddition[10] = { 0 };
+
+    short rightFirstAddition[10] = {0, 0, 0, 1, 1, 0, 1, 1, 1, 0 };
+    short rightSecondAddition[10] = { 1, 1, 1, 0, 0, 0, 0, 1, 0, 0 };
+    short rightThirdAddition[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    binarySum(binaryFirst, binarySecond, firstAddition, 10);
+    binarySum(binarySecond, binaryThird, secondAddition, 10);
+    binarySum(binaryFirst, binaryThird, thirdAddition, 10);
+
+    return correspondenceTest(binaryFirst, rightFirst, 10) && correspondenceTest(binarySecond, rightSecond, 10) && correspondenceTest(binaryThird, rightThird, 10) && \
+        correspondenceTest(firstAddition, rightFirstAddition, 10) && correspondenceTest(secondAddition, rightSecondAddition, 10) && correspondenceTest(thirdAddition, rightThirdAddition, 10) && \
+        decimalRepresentation(firstAddition, 10) == 110 && decimalRepresentation(secondAddition, 10) == -124 && decimalRepresentation(thirdAddition, 10) == 0;
 }
 
 int main(void) {
-	setlocale(LC_ALL, ".1251");
-	if (!(test1() && test2() && test3() && incorrectCase())) {
-		printf("Тесты не пройдены!");
-		return -1;
-	}
-	printf("Тесты успешно пройдены!\n");
-	printf("Введите два целых числа в диапазоне от -1073741823 до 1073741823 => ");
-	long long int firstNumber = verificationIntScanf();
-	while (firstNumber > 1073741823 || firstNumber < -1073741823) {
-		printf("Введено неверное значение. Повторите попытку => ");
-		firstNumber = verificationIntScanf();
-	}
-	long long int secondNumber = verificationIntScanf();
-	while (secondNumber > 1073741823 || secondNumber < -1073741823) {
-		printf("Введено неверное значение. Повторите попытку => ");
-		secondNumber = verificationIntScanf();
-	}
-	long long int biteToFirst = (int)floor(log2(2 * abs(firstNumber) + (firstNumber == 0)) / 8) + 2;
-	long long int biteToSecond = (int)floor(log2(2 * abs(secondNumber) + (secondNumber == 0)) / 8) + 2;
-	long long int maxBite = max(biteToFirst, biteToSecond);
-	if (firstNumber < 0) {
-		firstNumber += pow(256, maxBite);
-	}
-	if (secondNumber < 0) {
-		secondNumber += pow(256, maxBite);
-	}
-	long long int firstMask = pow(256, maxBite) / 2;
-	long long int secondMask = firstMask;
-	printf(" \n");
-	for (int i = 0; i < 8 * maxBite; ++i) {
-		if (i != 0 && i % 8 == 0) {
-			printf(" ");
-		}
-		printf("%s", firstNumber & firstMask ? "1" : "0");
-		firstMask >>= 1;
-	}
-	printf("\n");
-	for (int i = 0; i < 8 * maxBite; ++i) {
-		if (i != 0 && i % 8 == 0) {
-			printf(" ");
-		}
-		printf("%s", secondNumber & secondMask ? "1" : "0");
-		secondMask >>= 1;
-	}
-	printf("\n");
-	for (int i = 0; i < 9 * maxBite; ++i) {
-		printf("-");
-	}
-	printf("\n");
-	printf("\nСумма в десятичной системе счисления: %lli", additionOfTwoBinary(firstNumber, secondNumber, maxBite, true));
-	return 0;
+    setlocale(LC_ALL, ".1251");
+    if (!(test())) {
+        printf("Тесты не пройдены!");
+        return -1;
+    }
+    printf("Тесты успешно пройдены!\n");
+    printf("Введите два целых числа => ");
+    const int firstNumber = verificationIntScanf();
+    short firstBinary[BINARY_SIZE] = { 0 };
+    binaryRepresentation(firstNumber, firstBinary, BINARY_SIZE);
+    const int secondNumber = verificationIntScanf();
+    short secondBinary[BINARY_SIZE] = { 0 };
+    binaryRepresentation(secondNumber, secondBinary, BINARY_SIZE);
+    printf("Первое число: \n");
+    printBinary(firstBinary, BINARY_SIZE);
+    printf("Второе число: \n");
+    printBinary(secondBinary, BINARY_SIZE);
+    short sum[BINARY_SIZE] = { 0 };
+    printf("Сумма двух чисел: \n");
+    binarySum(firstBinary, secondBinary, sum, BINARY_SIZE);
+    printBinary(sum, BINARY_SIZE);
+    printf("Сумма в десятичном: %d", decimalRepresentation(sum, BINARY_SIZE));
+    return 0;
 }
 
